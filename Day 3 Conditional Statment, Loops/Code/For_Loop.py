14b6d48c4dd5d0008c1b514916e490f3841e9dae
Ayub <ayub.quadri89@gmail.com>
2017-10-11 09:06:57 +0530

    Class material for Day 3 and Day 4 Data

---
+++ Day 3 Conditional Statment, Loops/Code/For_Loop.py
@@ -0,0 +1,13 @@
+'''
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
+'''
+
+# for loop to print 100 times
+for x in range(0,6):
+    print x
+
+
+while x < 5:
+    print x 
