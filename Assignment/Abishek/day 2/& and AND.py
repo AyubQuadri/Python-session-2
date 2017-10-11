98c4cfdd4bca5e8400430416616ab55b40e82daa
abishek28 <acm2889@gmail.com>
2017-10-11 08:42:37 +0530

    Day2 assignment

---
+++ Assignment/Abishek/day 2/& and AND.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  8 22:47:29 2017
+
+@author: abishekcm
+"""
+
+print('Enter the value of a')
+a=input()
+a=int(a)
+print ('Enter the value of b')
+b=input()
+b=int(b)
+c= a & b
+print ('The & operation of a and b is',c)
+x= True
+y=False
+z= x and y
+print('The AND operation of x and y is',z)
---
+++ Assignment/Abishek/day 2/Logical.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  8 23:08:16 2017
+
+@author: abishekcm
+"""
+
+x=True
+y=False
+a= x and y
+print('The AND operation of x and y is',a)
+b= x or y
+print('The OR operation of x and y is',b)
+c= not y
+print('The NOT operation of  y is',c)
